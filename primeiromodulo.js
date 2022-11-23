3057755f16b20e6d1e577b662eb527c8982e1f38
Matheus Bispo <117827153+soumatheusbispo@users.noreply.github.com>
2022-11-23 10:57:41 -0300

    Aula 1

---
+++ primeiromodulo.js
@@ -0,0 +1,4 @@
+exports.nome = function () {
+    nome = 'Bispo';
+    return nome;
+};
---
+++ teste.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var n = require('./primeiromodulo');
+
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<b>Matheus dos Santos Bispo<b> <br>");
+    res.write("<i>Matheus </i>");
+    res.write(n.nome())
+    res.write(dia.diaDaSemana());
+
+    
+    res.end('Hello World!');
+  
+
+}).listen(8080);
